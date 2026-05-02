298c3f72236144d66d4223e54267068a7ccb3073
tawananyasha marekwa <tawananyashamarekwa@gmail.com>
2026-05-03 00:27:24 +0200

    fix cors headers at top

--- public/index.php
+++ public/index.php
@@ -2,20 +2,22 @@
 
 
 
+// CORS HEADERS - MUST BE FIRST
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
 $allowedOrigins = [
     'http://localhost:5173',
     'https://harareelectronichub.vercel.app'
 ];
-
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
 if (in_array($origin, $allowedOrigins)) {
     header("Access-Control-Allow-Origin: $origin");
 }
 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
