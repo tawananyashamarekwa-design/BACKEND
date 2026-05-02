acf07e4bdd79deb3363e89de40e54cd9d919e5d0
tawananyasha marekwa <tawananyashamarekwa@gmail.com>
2026-05-02 23:10:36 +0200

    add Router file

--- public/index.php
+++ public/index.php
@@ -61,7 +61,7 @@ ini_set('default_charset', 'utf-8');
 // Load core framework classes
 
 require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../core/router.php';
+require_once __DIR__ . '/../Core/router.php';
 
 // ============================================================================
 // LOAD UTILITY CLASSES
