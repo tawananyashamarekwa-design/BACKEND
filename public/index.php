0f671371e1a643ef970b95acca249a818164c301
tawananyasha marekwa <tawananyashamarekwa@gmail.com>
2026-05-03 00:23:22 +0200

    final working index routes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,27 @@
 <?php
 
+
+
+$allowedOrigins = [
+    'http://localhost:5173',
+    'https://harareelectronichub.vercel.app'
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
+
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 require_once __DIR__ . '/../config/app.php';
 require_once __DIR__ . '/../config/database.php';
 
