057dc220781d67a1d606c47cdd66a94d0d026342
tawananyasha marekwa <tawananyashamarekwa@gmail.com>
2026-05-02 23:39:46 +0200

    test correct index file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+die("THIS IS THE FILE I AM EDITING");
 require_once __DIR__ . '/../config/app.php';
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../Core/router.php';
