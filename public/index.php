5ba76bbb856636f053976c6bd470b34902643c16
tawananyasha marekwa <tawananyashamarekwa@gmail.com>
2026-05-03 16:32:06 +0200

    fix docker start command routing

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
 
+header('Access-Control-Allow-Origin: https://harareelectronichub.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // CORS HEADERS - MUST BE FIRST
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
